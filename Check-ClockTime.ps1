2d4a6a9a37f7918e82b328b2510efb15f1605752
Henrique Queiroz De Oliveira <henrique.queiroz@sp01.local>
2019-12-11 16:53:10 -0300

    add check-clocktime

---
+++ Check-ClockTime.ps1
@@ -0,0 +1,27 @@
+﻿    <#
+    .SYNOPSIS
+         Obtem a hora certa do servidor web e verifica se o horario atual está correto.
+    #>
+
+$web = Invoke-WebRequest -Method Get -Uri "https://uol.com" -UseBasicParsing
+[datetime]$onlineDate = $web.Headers.Date
+$diff = $(get-date) - $onlineDate
+$syncTime = $diff.TotalMinutes
+if ($syncTime -lt 0){$syncTime = $syncTime *-1}
+if ($syncTime -lt 4){
+    $horario = $true
+}
+else{
+    $horario = $false
+}
+$info = [pscustomobject]@{
+    "Hostname" = $(Get-ItemProperty -Path HKLM:\SYSTEM\ControlSet001\Services\Tcpip\Parameters).hostname
+    "Horario"  = $horario
+}
+$info | ConvertTo-Json
+
+if ($info.Horario -eq $false) {
+    $OUT = w32tm /resync
+    $OUT = start-sleep -Seconds 10
+    $OUT = w32tm /resync   
+}
